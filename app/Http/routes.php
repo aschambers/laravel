6fdf39295a51dbada2f056f7500f394057d7b32f
Alan Schambers <aschambers4291@gmail.com>
2017-10-28 13:19:00 -0700

    work on routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// past the post number to route
+Route::get('/post/{id}', function($id){
+	return "This is post number " . $id;
+});
+
+Route::get('/admin/posts/example', array('as'=>'admin.home', function() {
+	$url = route('admin.home');
+	return "this url is " . $url;
+}));
+
+Route::get('/post', 'PostsController@index');
